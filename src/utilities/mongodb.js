66e6504db82693898e84857b10c808c043924601
iamhuunter <131743511+iamhuunter@users.noreply.github.com>
2025-04-28 13:25:43 +0300

    Create mongodb.js

---
+++ src/utilities/mongodb.js
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
+}
+
+let client;
+let clientPromise;
+
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  global._mongoClientPromise = client.connect();
+}
+clientPromise = global._mongoClientPromise;
+
+export default clientPromise;
